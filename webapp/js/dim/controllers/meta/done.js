ef27dbb9717b2d5a2ea7524de07301690947143b
Peter Parente <parente@cs.unc.edu>
2013-04-21 20:56:20 -0400

    Fix #10, reset game on tap after win/lose

--- webapp/js/dim/controllers/meta/done.js
+++ webapp/js/dim/controllers/meta/done.js
@@ -1,13 +1,26 @@
 define([
-    'dim/topic'
-], function(topic) {
+    'dim/topic',
+    'dim/controllers/dpad'
+], function(topic, DPad) {
     var exports = {};
+
+    var on_tap = function() {
+        topic('reset').publish();
+    };
+
+    exports.destroy = function() {
+        topic('input.tap').unsubscribe(on_tap);
+    };
 
     exports.create = function(world, id) {
         var ctrl = world.get_ctrl(id);
         if(ctrl && ctrl.report) {
             topic('controller.report').publish(world, ctrl.report);
         }
+
+        // reset entire game
+        topic('input.tap').subscribe(on_tap);
+
         return this;
     };
 
